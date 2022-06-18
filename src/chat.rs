2d4b105c0c3b20d7313d7b252aa07c922807a6c6
Braden Mabey <braden.mabey@gmail.com>
2022-06-18 17:39:32 +0200

    Finished for now.

--- src/chat.rs
+++ src/chat.rs
@@ -14,20 +14,53 @@ enum TimeOfDay {
     Night(u8)
 }
 
-fn main_loop(user: &mut User) {
+fn main_loop(mut user: User) {
     //quick save in case it is a new user.
     save(&user);
     loop {
         //now ready to make the beef of this app.
         //No idea what it is for though.
         //Daily tracker? - probably
+        println!("Please start to be asked some questions.\nEnter help for other commands.");
 
         match read_input() {
             Input::Input(text) => {
                 match text.trim() {
+                    "start" | "Start" => {
+                        ask_questions(&mut user);
+                        println!("All questions asked.")
+                    },
+                    "print" | "Print" => print_answers(&mut user),
+                    "help" | "Help" => println!("{}", HELP),
                     "save" | "Save" => save(&user),
-                    "quit" | "Quit" => break,
+                    "quit" | "Quit" => {
+                        save(&user);
+                        break
+                    },
                     _ => println!("IDK")
+                }
+            }
+        }
+    }
+}
+
+fn print_answers(user: &mut User) {
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    for i in 0..user.question_data.len() {
+        println!("Question: {}", user.question_data[i].question);
+        println!("Answer: {}", user.question_data[i].answer)
+    }
+}
+
+fn ask_questions(user: &mut User){
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    for i in 0..user.question_data.len() {
+        if !user.question_data[i].asked {
+            println!("{}", user.question_data[i].question);
+            match read_input() {
+                Input::Input(text) => {
+                    user.question_data[i].answer = String::from(text.trim());
+                    user.question_data[i].asked = true;
                 }
             }
         }
@@ -47,7 +80,7 @@ pub fn start() {
                         Ok(_) => {
                             println!("welcome, {}", user.username);
                             question::load_questions(&mut user);
-                            main_loop(&mut user)
+                            main_loop(user)
                         },
                         Err(e) => {
                             match e {
@@ -65,7 +98,7 @@ pub fn start() {
                                                 "y" | "Y" => {
                                                     user.create();
                                                     question::load_questions(&mut user);
-                                                    main_loop(&mut user)
+                                                    main_loop(user)
                                                 },
                                                 _ => println!("Ok see you later.")
                                             }
@@ -126,9 +159,8 @@ fn save(user: &User) {
     let ser = serde_json::to_string_pretty(&user).unwrap();
     let mut out_path = String::from("./users/.json");
     out_path.insert_str(8, &user.username);
+    dbg!("Saved");
     fs::write(out_path, &ser).unwrap();
 }
 
-fn quit() {
-    todo!()
-}
+const HELP: &str = "I have no idea what this is.\nSave to save.\nQuit to quit.\nPrint to print your previous answers.";
